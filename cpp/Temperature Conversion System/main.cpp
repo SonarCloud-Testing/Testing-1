abdb56d8f804e7bac5457c8895fce9871eef062f
Vidhi-ORG <vidhivaghela2503@gmail.com>
2025-01-21 17:39:32 +0530

    Create main.cpp

---
+++ cpp/Temperature Conversion System/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class TemperatureConverter {
+public:
+    static double toCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+
+    static double toFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    static double convertTemperature(double temp, string unit) {
+        if (unit == "C") {
+            return toFahrenheit(temp);
+        } else if (unit == "F") {
+            return toCelsius(temp);
+        } else {
+            cout << "Invalid unit!" << endl;
+            return -1;  // Invalid value returned
+        }
+    }
+};
+
+int main() {
+    double temperature;
+    string unit;
+    cout << "Enter temperature and unit (C or F): ";
+    cin >> temperature >> unit;
+    
+    double result = TemperatureConverter::convertTemperature(temperature, unit);
+    cout << "Converted temperature: " << result << endl;
+
+    return 0;
+}
